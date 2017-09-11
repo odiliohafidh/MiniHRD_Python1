38cd849384e3b35e5e4a09820c73a8bc06b25d32
odiliohafidh <email>
2017-09-11 14:44:30 +0700

    push versi 1

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # MiniHRD_Python1
+aplikasi web pyhton sederhana dengan django
+https://www.codepolitan.com/membangun-aplikasi-mini-hrd-django-pengenalan-django-admin
---
+++ kehadiran/admin.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+from kehadiran.models import Kehadiran
+# Register your models here.
+class KehadiranAdmin (admin.ModelAdmin):
+    list_display = ['karyawan', 'jenis_kehadiran', 'waktu']
+    list_filter = ('jenis_kehadiran',)
+    search_fields = []
+    list_per_page = 25
+
+admin.site.register(Kehadiran, KehadiranAdmin)
